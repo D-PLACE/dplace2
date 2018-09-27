79b90f44b7ac7dbfc509e51c1e2ff09f40c943fe
xrotwang <xrotwang@googlemail.com>
2018-09-27 14:25:06 +0200

    closes #7

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         'clld>=4.4.0',
         'clldmpg>=3.3.1',
-        'clld_phylogeny_plugin>=1.0.1',
+        'clld_phylogeny_plugin>=1.2.0',
         'sqlalchemy',
         'waitress',
     ],
